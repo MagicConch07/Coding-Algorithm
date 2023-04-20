baf278e352afe32d30e80901d9903a922ffe9419
minkyo <123707368+Potato0Tomato@users.noreply.github.com>
2023-04-20 19:14:12 +0900

    [Bronze V] Title: A+B - 3, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int jul;
+	int a, b;
+
+	cin >> jul;
+
+	for (int i = 0; i < jul; i++) {
+		
+		cin >> a >> b;
+		cout << a + b << endl;
+	}
+}	
+
+
+	
