a76926f01032dd9045ddc9258a23eaef44f1acdb
minkyo <123707368+Potato0Tomato@users.noreply.github.com>
2023-09-19 18:45:24 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> array) {
+    vector<int> answer;
+    int max = 0;
+    
+    for(int i = 0; i < array.size(); i++){
+        if(array[i] > max)
+            max = array[i];
+    }
+    
+    answer.push_back(max);
+    
+    for(int i = 0; i < array.size(); i++){
+        if(array[i] == max)
+            answer.push_back(i);
+    }
+    
+    return answer;
+}
