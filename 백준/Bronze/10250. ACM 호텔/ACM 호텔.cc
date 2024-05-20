52f7716318f637f17df512cbc97bbcf3723d96e5
minkyo <123707368+MagicConch07@users.noreply.github.com>
2024-05-21 00:28:01 +0900

    [Bronze III] Title: ACM 호텔, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.cc
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.cc
@@ -1,2 +1,2 @@
 #include <iostream>
-using namespace std;int main(){int t,h,w,n=0;cin>>t;for(int i=0;i<t;++i){int slice=1;cin>>h>>w>>n;while(h<n){if(h<n){n-=h;slice++;}}cout<<(slice+(n*100))<<endl;}}
+using namespace std;int main(){int t,h,w,n;cin>>t;for(int i;i<t;++i){int s=1;cin>>h>>w>>n;while(h<n){if(h<n){n-=h;s++;}}cout<<(s+(n*100))<<"\n";}}
